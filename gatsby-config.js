b49a5b6545c80d8f3ff90e20ed67af2e4ef2ccb5
Aleksei Ustiuzhanin <elanhant@gmail.com>
2019-10-13 21:14:06 -0700

    Update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: '"MasterQSA PCI Compliance"',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'MasterQSA improves the PCI compliance report creation process for both professional security assessors and the employees of the merchant being assessed',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
