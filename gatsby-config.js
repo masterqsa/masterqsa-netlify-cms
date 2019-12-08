aa1b285959f7f94684205c115d1774b0ed0320c2
Victor Isaev <victor@torrio.net>
2019-12-08 15:43:39 -0800

    Change verbiage for page title defaults - MasterQSA PCI Compliance

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: '"MasterQSA PCI Compliance"',
+    title: 'MasterQSA PCI Compliance',
     description:
       'MasterQSA improves the PCI compliance report creation process for both professional security assessors and the employees of the merchant being assessed',
   },
